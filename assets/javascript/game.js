945830ea46ad09fe200e214fb3763ccb5a219ab0
AntonSemenov <AntonSemenov@Antons-MacBook-Pro.local>
2018-08-07 18:10:33 -0500

    updated codes

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+//random number is created (19 - 120)
+var compRandomNumber = Math.floor(Math.random() * 120 + 19);
+console.log(compRandomNumber);
+
+var crystalRandomNumber = Math.floor(Math.random() * 12 + 1);
+console.log(crystalRandomNumber);
+
